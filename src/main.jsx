073ad6a9f3f5d721008137b9b4d829e785dd5e62
אמית דיגיטל <mopamit24@gmail.com>
2026-07-16 11:07:24 +0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CompoundsLab from './CompoundsLab.jsx';
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CompoundsLab />
+  </React.StrictMode>
+);
